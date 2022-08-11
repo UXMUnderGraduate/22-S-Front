958c46ff8446c718f94ba56517864ad1c208cd79
MinsuKim <minsu020302@gmail.com>
2022-08-11 16:30:45 +0900

    Feat : 헤더 구현

---
+++ src/components/Header.jsx
@@ -0,0 +1,162 @@
+import React from 'react';
+// import { useNavigate } from 'react-router-dom';
+import { styled, alpha } from '@mui/material/styles';
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  InputBase,
+  Badge,
+  MenuItem,
+  Menu,
+  ListItemButton,
+  Button,
+} from '@mui/material/';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MailIcon from '@mui/icons-material/Mail';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
+export default function Header() {
+  // const Navigate = useNavigate();
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+      <MenuItem
+      // onClick={() => {
+      //   Navigate('/login');
+      // }}
+      >
+        Log out
+      </MenuItem>
+    </Menu>
+  );
+  return (
+    <Box>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <Button
+          // onClick={() => {
+          //   Navigate('/home');
+          // }}
+          >
+            <Box src="logo/logo_transparent.png" component="img" sx={{ width: 50 }} />
+          </Button>
+          <Box sx={{ display: 'inline-' }}>
+            <ListItemButton
+            // onClick={() => {
+            //   Navigate('/home');
+            // }}
+            >
+              Home
+            </ListItemButton>
+            <ListItemButton
+            // onClick={() => {
+            //   Navigate('/library');
+            // }}
+            >
+              Library
+            </ListItemButton>
+          </Box>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase placeholder="Search…" inputProps={{ 'aria-label': 'search' }} />
+          </Search>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box>
+            <IconButton size="large" aria-label="badgeContent 값을 바꿔 알림수를 알려준다" color="inherit">
+              <Badge badgeContent={10} color="error">
+                <MailIcon />
+              </Badge>
+            </IconButton>
+            <IconButton size="large" aria-label="badgeContent 값을 바꿔 알림수를 알려준다" color="inherit">
+              <Badge badgeContent={14} color="error">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {renderMenu}
+    </Box>
+  );
+}
