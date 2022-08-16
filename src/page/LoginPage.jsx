dde853021df5e2221f40b8b59f872c954e91d5b5
choijihyeon <gus1043@gmail.com>
2022-08-16 10:38:24 +0900

    feat:axios 변경 #6

--- src/page/LoginPage.jsx
+++ src/page/LoginPage.jsx
@@ -1,12 +1,58 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import TextField from '@mui/material/TextField';
 import Checkbox from '@mui/material/Checkbox';
 import Button from '@mui/material/Button';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Link from '@mui/material/Link';
 import { Grid, Typography, Box, Container } from '@mui/material';
+import axios from 'axios'
 
 function LoginPage () {
+  const [inputEmail, setInputEmail] = useState('')
+  const [inputPw, setInputPW] = useState('')
+
+  const handleInputEmail = (e) => {
+    setInputEmail(e.target.value)
+  }
+
+  const handleInputPw = (e) => {
+    setInputPW(e.target.value)
+  }
+
+  //로그인
+  const onClickLogin = () => {
+    console.log('로그인');
+    console.log('Email:', inputEmail);
+    console.log('Pw:', inputPw);
+    axios.post('http://localhost:5000/api/v1/auth/signin', null, {
+      params: {
+        'email' : inputEmail,
+        'password' : inputPw
+      }
+    })
+    .then(res => {
+      if(res.data.email === undefined){
+        //이메일 불일치
+        alert('아이디를 확인하세요.')
+      } else if (res.data.email===null){
+        //비밀번호 불일치
+        alert('비밀번호를 확인하세요.')
+      }else if(res.data.email===inputEmail){
+        //일치
+        sessionStorage.setItem('email', inputEmail)
+      }
+      //로그인 성공시 메인페이지로 이동
+      document.location.href ='/'
+    })
+    .catch()
+  }
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/v1/auth/signin')
+    .then(res => console.log(res))
+    .catch()
+},[])
+
 
   return (
     <div>
@@ -29,7 +75,9 @@ function LoginPage () {
         autoComplete='email'
         required 
         fullWidth
-        autoFocus/>
+        autoFocus
+        onChange={handleInputEmail}
+        value={inputEmail}/>
 
       <TextField 
         margin='normal'
@@ -38,12 +86,15 @@ function LoginPage () {
         type="password"
         autoComplete='current-password'
         required 
-        fullWidth/>
+        fullWidth
+        onChange={handleInputPw}
+        value={inputPw}/>
+      
         
       <FormControlLabel
         control={<Checkbox value="remember" color="primary" />}
         label="자동로그인"/>
-      <Button type="submit" fullWidth variant='contained' sx={{ mt: 3, mb:2 }} style={{backgroundColor:"#7A84EB", height:'60px', fontSize:"20px"}}> 로그인 </Button>
+      <Button type="button" fullWidth onSubmit={onClickLogin} variant='contained' sx={{ mt: 3, mb:2 }} style={{backgroundColor:"#7A84EB", height:'60px', fontSize:"20px"}}> 로그인 </Button>
       <Grid container>
         <Grid item xs> <Link href="/register" style={{color : 'gray' , textDecoration: 'none'}}> 회원가입 </Link> </Grid>
         <Grid item> <Link href="/" style={{color : 'gray' , textDecoration: 'none'}}> 메인으로 이동 </Link> </Grid>
