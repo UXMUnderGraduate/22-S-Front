5c8814096277ea2a50556a372e0ce773042586b0
MinsuKim <minsu020302@gmail.com>
2022-08-18 13:01:55 +0900

    Design : 화면 레이아웃디자인 변경#6

--- src/page/LoginPage.jsx
+++ src/page/LoginPage.jsx
@@ -1,59 +1,105 @@
-import React from 'react'
+import React from 'react';
 import TextField from '@mui/material/TextField';
 import Checkbox from '@mui/material/Checkbox';
 import Button from '@mui/material/Button';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Link from '@mui/material/Link';
-import { Grid, Typography, Box, Container } from '@mui/material';
+import { Grid, Typography, Box, Container, createTheme, ThemeProvider } from '@mui/material';
 
-function LoginPage () {
-
+function LoginPage() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#550064',
+      },
+      secondary: {
+        main: '#7966ce',
+      },
+    },
+  });
   return (
-    <div>
-      <Container component="main" maxWidth="xs">
-      <Box
-        sx={{
-          marginTop: 35,
-          display: 'flex',
-          flexDirection: 'column'
-          }}
-        >
-      <Typography component='h1' varient='h1' style={{textAlign:"center", fontSize:"30px", fontWeight:'bold', marginBottom:"10px"}}>
-        Login
-      </Typography>
-      <TextField 
-        margin='normal'
-        label='Email Address' 
-        name='email'
-        type='email'
-        autoComplete='email'
-        required 
-        fullWidth
-        autoFocus/>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ height: '100vh', backgroundImage: 'url(/images/background.png)' }}>
+        <Container component="main" maxWidth="xs">
+          <Box
+            sx={{
+              paddingTop: 35,
+              display: 'flex',
+              flexDirection: 'column',
+            }}
+          >
+            <Typography
+              component="h1"
+              varient="h1"
+              style={{ textAlign: 'center', fontSize: '30px', fontWeight: 'bold', marginBottom: '10px' }}
+            >
+              Login
+            </Typography>
+            <TextField
+              sx={{ backgroundColor: 'white' }}
+              color="secondary"
+              margin="normal"
+              label="Email Address"
+              name="email"
+              type="email"
+              autoComplete="email"
+              required
+              fullWidth
+              autoFocus
+            />
+            <TextField
+              sx={{ backgroundColor: 'white' }}
+              color="secondary"
+              margin="normal"
+              label="Password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              required
+              fullWidth
+            />
 
-      <TextField 
-        margin='normal'
-        label="Password"
-        name="password" 
-        type="password"
-        autoComplete='current-password'
-        required 
-        fullWidth/>
-        
-      <FormControlLabel
-        control={<Checkbox value="remember" color="primary" />}
-        label="자동로그인"/>
-      <Button type="submit" fullWidth variant='contained' sx={{ mt: 3, mb:2 }} style={{backgroundColor:"#7A84EB", height:'60px', fontSize:"20px"}}> 로그인 </Button>
-      <Grid container>
-        <Grid item xs> <Link href="/register" style={{color : 'gray' , textDecoration: 'none'}}> 회원가입 </Link> </Grid>
-        <Grid item> <Link href="/" style={{color : 'gray' , textDecoration: 'none'}}> 메인으로 이동 </Link> </Grid>
-      </Grid>
+            <FormControlLabel
+              sx={{ color: '#7966ce' }}
+              control={
+                <Checkbox
+                  value="remember"
+                  sx={{ '& .MuiSvgIcon-root': { fontSize: 28 }, color: 'white', '&.Mui-checked': { color: '#7966ce' } }}
+                />
+              }
+              label="자동로그인"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              style={{ backgroundColor: '#7966ce', height: '60px', fontSize: '20px' }}
+            >
+              {' '}
+              로그인{' '}
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                {' '}
+                <Link href="/register" style={{ color: '#B73DCC', textDecoration: 'none' }}>
+                  {' '}
+                  회원가입{' '}
+                </Link>{' '}
+              </Grid>
+              <Grid item>
+                {' '}
+                <Link href="/" style={{ color: '#B73DCC', textDecoration: 'none' }}>
+                  {' '}
+                  메인으로 이동{' '}
+                </Link>{' '}
+              </Grid>
+            </Grid>
+          </Box>
+        </Container>
       </Box>
-      </Container>
-      
-
-    </div>
-  )
+    </ThemeProvider>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
