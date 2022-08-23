3a1832830791a08d054195611e7b0a05775f5a45
MinsuKim <minsu020302@gmail.com>
2022-08-23 15:13:57 +0900

    Feat : 약관 모달 창 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -120,29 +120,27 @@ export default function Header() {
     >
       <MenuItem onClick={() => Navigate('/profile')}>My account</MenuItem>
       <MenuItem onClick={() => Navigate('/upload')}>Upload</MenuItem>
-      <MenuItem onClick={handleOpen}>
-        terms of service
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={style}>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-              Terms of service
-            </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-              industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-              scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
-              of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
-              like Aldus PageMaker including versions of Lorem Ipsum.
-            </Typography>
-          </Box>
-        </Modal>
-      </MenuItem>
+      <MenuItem onClick={handleOpen}>terms of service</MenuItem>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Terms of service
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry
+            s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it
+            to make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+            typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+            sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </Typography>
+        </Box>
+      </Modal>
       <MenuItem>Log out</MenuItem>
     </Menu>
   );
