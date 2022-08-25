3c8dbd9c9c52ff8bdd2a3484166734f7a91699a4
choijihyeon <gus1043@gmail.com>
2022-08-25 12:50:10 +0900

    feat: login axios 연결

--- src/page/LoginPage.jsx
+++ src/page/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TextField from '@mui/material/TextField';
 import Checkbox from '@mui/material/Checkbox';
 import Button from '@mui/material/Button';
@@ -6,6 +6,7 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Link from '@mui/material/Link';
 import { Grid, Typography, Box, Container, createTheme, ThemeProvider } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 function LoginPage() {
   const theme = createTheme({
     palette: {
@@ -17,6 +18,49 @@ function LoginPage() {
       },
     },
   });
+  const [inputEmail, setInputEmail] = useState('')
+  const [inputPw, setInputPW] = useState('')
+
+  const handleInputEmail = (e) => {
+    setInputEmail(e.target.value)
+  }
+
+  const handleInputPw = (e) => {
+    setInputPW(e.target.value)
+  }
+
+  const onClickLogin = () => {
+    console.log('로그인');
+    console.log('Email:', inputEmail);
+    console.log('Pw:', inputPw);
+    axios.post('http://localhost:5000/api/v1/auth/signin', null, {
+      params: {
+        'email' : inputEmail,
+        'password' : inputPw
+      }
+    })
+    .then(res => {
+        console.log(res)
+        console.log('res.data.userId :: ', res.data.userId)
+        console.log('res.data.msg :: ', res.data.msg)
+        if(res.data.email === undefined){
+            // id 일치하지 않는 경우 userId = undefined, msg = '입력하신 id 가 일치하지 않습니다.'
+            console.log('======================',res.data.msg)
+            alert('입력하신 id 가 일치하지 않습니다.')
+        } else if(res.data.email === null){
+            // id는 있지만, pw 는 다른 경우 userId = null , msg = undefined
+            console.log('======================','입력하신 비밀번호 가 일치하지 않습니다.')
+            alert('입력하신 비밀번호 가 일치하지 않습니다.')
+         } else if(res.data.email === inputEmail) {
+             // id, pw 모두 일치 userId = userId1, msg = undefined
+             console.log('======================','로그인 성공')
+            sessionStorage.setItem('user_email', inputEmail)
+        }
+        // 작업 완료 되면 페이지 이동(새로고침)
+         document.location.href = '/'
+     })
+    .catch()
+  }
   const Navigate = useNavigate();
   return (
     <ThemeProvider theme={theme}>
@@ -47,7 +91,8 @@ function LoginPage() {
               required
               fullWidth
               autoFocus
-            />
+              onChange={handleInputEmail}
+              value={inputEmail}/>
             <TextField
               sx={{ backgroundColor: 'white', borderRadius: '0.5em' }}
               color="secondary"
@@ -58,6 +103,8 @@ function LoginPage() {
               autoComplete="current-password"
               required
               fullWidth
+              onChange={handleInputPw}
+              value={inputPw}
             />
 
             <FormControlLabel
@@ -75,6 +122,7 @@ function LoginPage() {
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
+              onClick={onClickLogin}
               style={{ backgroundColor: '#7966ce', height: '60px', fontSize: '20px' }}
             >
               로그인
