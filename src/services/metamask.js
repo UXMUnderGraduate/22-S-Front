bbbe9c17b49747dce0f4f6ddcd4bf0f59d50214e
dipito <77622897+Gi-Woong@users.noreply.github.com>
2022-08-31 01:04:05 +0900

    Refactor: MetaMask Injection 코드 최신화

--- src/services/metamask.js
+++ src/services/metamask.js
@@ -1,23 +1,33 @@
-import Web3 from 'web3';
+import Web3 from "web3";
 
 class Metamask {
   constructor() {
     this.web3 = null;
     this.web3Provider = null;
-    this.account = '0x0';
+    this.account = "0x0";
   }
 
   async init() {
-    console.log('Metamask.init()');
-    this.web3Provider = window.ethereum;
-    this.web3 = new Web3(this.web3Provider);
-
-    if (window.ethereum) {
-      // eslint-disable-next-line no-undef
-      await ethereum.enable();
-      this.account = this.web3.currentProvider.selectedAddress;
-      console.log('Your Account: ' + this.account);
+    console.log("Metamask.init()");
+    this.web3Provider = await detectEthereumProvider();
+    if (!this.web3Provider) {
+      console.log("Please install MetaMask!");
     }
+    await ethereum
+      .request({
+        method: "eth_requestAccounts",
+      })
+      .catch((error) => {
+        if (error.code === 4001) {
+          // EIP-1193 userRejectedRequest error
+          console.log("Please connect to MetaMask.");
+        } else {
+          console.error(error);
+        }
+      });
+    this.web3 = new Web3(this.web3Provider);
+    this.account = this.web3.currentProvider.selectedAddress;
+    console.log("Your Account: " + this.account);
   }
 }
 
