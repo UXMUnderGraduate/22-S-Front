203e868e48ac3c3a2a266a0283d64eeb57907401
choijihyeon <gus1043@gmail.com>
2022-09-20 01:37:37 +0900

    feat:confirm메세지로 중복클릭 차단

--- src/page/RegisterPage.jsx
+++ src/page/RegisterPage.jsx
@@ -82,6 +82,11 @@ function RegisterPage() {
   };
 
   const handleSubmit = (e) => {
+    if(confirm('회원가입하시겠습니까?')){
+      //form submit
+  }else{
+      return;
+  }
     e.preventDefault();
 
     const data = new FormData(e.currentTarget);
