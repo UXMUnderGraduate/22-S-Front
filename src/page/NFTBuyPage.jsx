b93c3e394f19ae8e9b25464fff7668a4671c1107
MinsuKim <minsu020302@gmail.com>
2023-02-12 23:15:09 +0900

    Feat : NFT구매페이지 완료

--- src/page/NFTBuyPage.jsx
+++ src/page/NFTBuyPage.jsx
@@ -1,11 +1,13 @@
 import { Box, CircularProgress, Typography, Button, Card, Select, MenuItem, FormControl } from '@mui/material';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
-const id = 1;
+// const id = 1;
 
 export default function NFTPage() {
+  const { state } = useLocation();
+  const { id } = state;
   const navigate = useNavigate();
   const [data, setData] = useState('');
   const [loading, setLoading] = useState(true);
@@ -15,14 +17,14 @@ export default function NFTPage() {
   // const [price, setPrice] = React.useState('');
 
   const [formData, setFormData] = React.useState({
-    celler: '',
-    price: '',
+    celler: 'test1',
+    price: 'WEI',
   });
 
   const handleChange = (e) => {
     const nextForm = {
       ...formData,
-      [e.target.id]: e.target.value,
+      [e.target.name]: e.target.value,
     };
     console.log(nextForm);
     setFormData(nextForm);
@@ -54,7 +56,7 @@ export default function NFTPage() {
 
   return (
     <Box sx={{ pt: '10%', height: '100vh', backgroundImage: 'url(/images/background.png)', textAlign: 'center' }}>
-      <Box sx={{ justifyContent: 'center', width: '70vw', display: 'flex', ml: '15%' }}>
+      <Box sx={{ justifyContent: 'center', display: 'flex' }}>
         <Box>
           {loading ? (
             <CircularProgress />
@@ -63,6 +65,7 @@ export default function NFTPage() {
               sx={{
                 width: '40vw',
                 height: '40vw',
+                ml:"4vw",
                 backgroundSize: 'cover',
                 backgroundImage: `url('data:image/jpeg;base64,${data.image}')`,
               }}
@@ -80,43 +83,47 @@ export default function NFTPage() {
           <FormControl sx={{ m: 1, width: '40vw' }}>
             <Select
               id="celler"
+              name="celler"
               variant="outlined"
-              value={'celler'}
-              label="celler"
+              displayEmpty
+              value={formData.celler}
+              label={'판매자선택'}
               onChange={handleChange}
-              sx={{ borderRadius: '0.3em', mb: '2%', bgcolor: '#f0f0f0' }}
+              sx={{ borderRadius: '0.3em', mb: '2%', bgcolor: 'white', color: 'black' }}
             >
-              <MenuItem value={'test'}>판매자1</MenuItem>
-              <MenuItem value={'test'}>판매자2</MenuItem>
-              <MenuItem value={'test'}>판매자3</MenuItem>
+              <MenuItem value={'test1'}>판매자1</MenuItem>
+              <MenuItem value={'test2'}>판매자2</MenuItem>
+              <MenuItem value={'test3'}>판매자3</MenuItem>
             </Select>
             <Select
               id="price"
+              name="price"
               variant="outlined"
-              value={'price'}
-              label="price"
+              value={formData.price}
+              label={'단위선택'}
               onChange={handleChange}
-              sx={{ borderRadius: '0.3em', bgcolor: '#f0f0f0' }}
+              sx={{ borderRadius: '0.3em', mb: '2%', bgcolor: 'white', color: 'black' }}
             >
-              <MenuItem value={'test'}>WEI</MenuItem>
-              <MenuItem value={'test'}>ETH</MenuItem>
+              <MenuItem value={'WEI'}>WEI</MenuItem>
+              <MenuItem value={'ETH'}>ETH</MenuItem>
             </Select>
+            <Typography variant="h6" component="h5">
+              {'NFT Price : ' + '0 ETH'}
+            </Typography>
+            {localStorage.getItem('type') === 'General' ? (
+              <Button
+                onClick={async () => {
+                  await console.log('구매함수 쓰셈');
+                  //NFT 구매함수 여기 console.log 지우고 쓰셈 nft구매에 필요한 데이터는 ui바꿀때 콘솔에 찍히거든 객체담기는 변수는 formData고 필요한거잇으면 카톡 ㄱㄱ
+                }}
+                variant="contained"
+                color="secondary"
+                sx={{ fontSize: '0.3rm', width: '70%', padding: '1vh', mt: '10%', ml: '15%'}}
+              >
+                NFT 구매
+              </Button>
+            ) : null}
           </FormControl>
-          <Typography variant="h6" component="h5">
-            {'NFT Price : ' + '0 ETH'}
-          </Typography>
-          {localStorage.getItem('type') === 'General' ? (
-            <Button
-              onClick={async () => {
-                await console.log('다른함수넣기');
-              }}
-              variant="contained"
-              color="secondary"
-              sx={{ fontSize: '0.3rm', width: '70%', padding: '1vh', mt: '10%' }}
-            >
-              NFT 구매
-            </Button>
-          ) : null}
         </Box>
       </Box>
     </Box>
