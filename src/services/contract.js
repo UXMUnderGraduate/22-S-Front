87c3bf02338081f7644b798549e3997ecb786215
dipito <rldndco@mju.ac.kr>
2023-06-26 15:39:09 +0900

    feat: abi, bin 추가에 따른 web3 api 추가

--- src/services/contract.js
+++ src/services/contract.js
@@ -1,16 +1,31 @@
-import Metamask from './metamask';
 import Web3 from 'web3';
+import Metamask from './metamask.js';
 
-import bytecodeSettle from '../assets/contracts_SettlementContract_sol_SettlementContract.txt';
-import abiSettle from '../assets/contracts_SettlementContract_sol_SettlementContract.json';
+let bytecodeSettle;
+let abiSettle;
+let bytecodeNFT;
+let abiNFT;
 
-const metamask = new Metamask();
-
-export const init = async () => {
-  await metamask.init();
+//index.html의 실행 위치에 따른 상대경로
+const importData = async () => {
+  bytecodeSettle = './assets/SettlementContractExtra.txt';
+  abiSettle = await fetch('./assets/SettlementContractExtra.json').then((res) => res.json());
+  bytecodeNFT = './assets/NFT1155.txt';
+  abiNFT = await fetch('./assets/NFT1155.json').then((res) => res.json());
 };
 
+export const metamask = new Metamask();
+
+// metamask 초기화 및 컴파일된 컨트랙트 데이터를 불러옴
+export const init = async () => {
+  const result = await metamask.init();
+  await importData();
+  return result;
+};
+
+// contract deploy에 관한 object
 export const deployContract = {
+  // 기본적인 deploy 함수 실행
   deploy: async (abi, bin, from, args = []) => {
     const data = await fetch(bin).then((res) => res.text());
     const contractInstance = new metamask.web3.eth.Contract(abi);
@@ -32,7 +47,16 @@ export const deployContract = {
         return newContractInstance;
       });
   },
+  // deploy settlementContractExtra contract
+  /**
+   * @param {Array} addresses - 저작권자들 각각의 지갑주소
+   * @param {Array} proportions - 저작권자들 각각의 정산 비율
+   * @param {String} songCid - CID1(곡 메타데이터 CID)
+   * @param {String | Number} price - 곡의 가격
+   * @return {Object} settlementContract 객체 반환
+   */
   settlement: async (addresses, proportions, songCid, price) => {
+    //deployContract.
     const bytes = [
       Web3.utils.padRight(Web3.utils.toHex(songCid.substr(0, 32)), 64),
       Web3.utils.padRight(Web3.utils.toHex(songCid.substr(32)), 64),
@@ -42,16 +66,36 @@ export const deployContract = {
     console.log(`Settlement contract deployed: ${deployedSettleContract.options.address}`);
     return deployedSettleContract;
   },
+  // deploy NFT1155 contract
+  nft: async (dir, contract) => {
+    //deployContract.
+    const args = [dir, contract];
+    console.log('in the nft : ' + args);
+    const deployedNftContract = await deployContract.deploy(abiNFT, bytecodeNFT, metamask.account, args);
+    console.log(`NFT contract deployed: ${deployedNftContract.options.address}`);
+    return deployedNftContract;
+  },
 };
 
+//settlementContractExtra contract의 method들과 state variable들에 접근하는 object
 export const settlementContract = {
+  /**
+   * settlementContract 객체
+   */
   instance: null,
+  /**
+   * 컨트랙트 address를 입력으로 받아 contract 객체를 load함.
+   * @param {string} settlementAddr settlementContract Address
+   */
   load: (settlementAddr) => {
     settlementContract.instance = new metamask.web3.eth.Contract(abiSettle, settlementAddr);
     settlementContract.instance.setProvider(metamask.web3Provider);
     console.log('Settlement contract loaded:');
     console.log(settlementContract.instance);
   },
+  /**
+   * buy()함수
+   */
   buy: async () => {
     const value = await settlementContract.instance.methods.price().call();
     return settlementContract.instance.methods.buy().send({
@@ -69,9 +113,16 @@ export const settlementContract = {
       from: metamask.account,
     });
   },
+
+  // get을 붙인 이유는 solidity에서 외부에서 contract를 참조할 경우
+  // state variable에 접근 할 수 있도록 getter가 자동으로 생성되기 때문
+  // ex) copyrightHolders의 getter: getCopyrightHolders()
   variables: {
-    getCopyrightHolders: async () => {
-      return settlementContract.instance.methods.copyrightHolders().call();
+    getCopyrightHolders: async (address) => {
+      return settlementContract.instance.methods.copyrightHolders(address).call();
+    },
+    getNftContractAddresses: async (address) => {
+      return settlementContract.instance.methods.getNftContractAddresses(address).call();
     },
     getCumulativeSales: async () => {
       return settlementContract.instance.methods.cumulativeSales().call();
@@ -110,8 +161,10 @@ export const settlementContract = {
           name: 'amount',
         },
       ];
-      const { data: hexString, topcis } = receipt.logs[0];
-      return metamask.web3.eth.abi.decodeLog(input, hexString, topcis);
+
+      console.log(receipt.logs[0]);
+      const { data: hexString, topics } = receipt.logs[0];
+      return metamask.web3.eth.abi.decodeLog(input, hexString, topics);
     },
     getBuyLog: async (result) => {
       const tx = result.transactionHash;
@@ -130,12 +183,70 @@ export const settlementContract = {
           name: 'amount',
         },
       ];
-      const { data: hexString, topcis } = receipt.logs[0];
-      return metamask.web3.eth.abi.decodeLog(input, hexString, topcis);
+      const { data: hexString, topics } = receipt.logs[0];
+      return metamask.web3.eth.abi.decodeLog(input, hexString, topics);
     },
   },
 };
 
+//NFT1155 contract의 method들과 state variable들에 접근하는 object
+export const nftContract = {
+  instance: null,
+  load: (nftAddr) => {
+    console.log('nftAddr : ' + nftAddr);
+    nftContract.instance = new metamask.web3.eth.Contract(abiNFT, nftAddr);
+    console.log('nftContract.instance : ' + nftContract.instance);
+    nftContract.instance.setProvider(metamask.web3Provider);
+    console.log('NFT contract loaded:');
+    console.log(nftContract.instance);
+  },
+  // add NFT1155's methods here.abiNFT
+  buy: async (value) => {
+    // const value = await nftContract.instance.methods.price().call();
+    return nftContract.instance.methods.buy().send({
+      from: metamask.account,
+      value: metamask.web3.utils.toWei(String(value), 'wei'),
+    });
+  },
+  sell: async (value) => {
+    return nftContract.instance.methods.sell(value).send({
+      from: metamask.account,
+    });
+  },
+  isCopyrightHolder: () => {
+    return nftContract.instance.methods.isCopyrightHolder().send({
+      from: metamask.account,
+    });
+  },
+  register: () => {
+    return nftContract.instance.methods.register().send({
+      from: metamask.account,
+    });
+  },
+  isApprovedForAll: (account, operator) => {
+    return nftContract.instance.methods.isApprovedForAll(account, operator).send({
+      from: metamask.account,
+    });
+  },
+  variables: {
+    // add NFT1155's state variable getters here.
+    getMinter: async () => {
+      return nftContract.instance.methods.minter().call();
+    },
+    getOwner: async () => {
+      return nftContract.instance.methods.owner().call();
+    },
+    getPrice: async () => {
+      return nftContract.instance.methods.price().call();
+    },
+    getProportion: async () => {
+      return nftContract.instance.methods.proportion().call();
+    },
+  },
+  //event는 사용하지 않으므로 필요 없음.
+};
+
+// 현재 metamask의 account(address)와 balance(잔액)을 logging
 export const logAccount = async () => {
   const account = metamask.account;
   const balance = await metamask.web3.eth.getBalance(account);
